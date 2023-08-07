09729db573eeebd5cb69aed2370234277d3b85d9
rougraff <140021677+rougraff@users.noreply.github.com>
2023-08-07 13:11:38 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired region
+}
+
+resource "aws_appsync_graphql_api" "example_api" {
+  name          = "ExampleAPI"
+  authentication_type = "API_KEY"
+}
+
+resource "aws_appsync_datasource" "example_datasource" {
+  name            = "ExampleDataSource"
+  api_id          = aws_appsync_graphql_api.example_api.id
+  type            = "NONE"
+}
+
+resource "aws_appsync_resolver" "example_resolver" {
+  api_id          = aws_appsync_graphql_api.example_api.id
+  type            = "Query"
+  field           = "hello"
+
+  data_source     = aws_appsync_datasource.example_datasource.name
+
+  request_template = jsonencode({
+    version: "2017-02-28",
+    operation: "GET",
+    path: "/hello"
+  })
+}
+
+resource "aws_appsync_type" "example_type" {
+  api_id = aws_appsync_graphql_api.example_api.id
+  name   = "ExampleType"
+
+  field {
+    name = "message"
+    type = "String"
+  }
+}
