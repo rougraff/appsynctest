42747900d2e19e93b42d8bba5351060685d59578
rougraff <140021677+rougraff@users.noreply.github.com>
2023-08-07 16:38:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,42 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Set the AWS credentials profile and region you want to publish to.
 provider "aws" {
-  profile = var.aws_credentials_profile
-  region  = var.region
+  region = "us-east-1"  # Replace with your desired AWS region
 }
 
-# --- AppSync Setup ---
+resource "aws_appsync_graphql_api" "sample_appsync_api" {
+  name                = "SampleAppSyncAPI"
+  authentication_type = "API_KEY"  # You can change the authentication method as needed
 
-# Create the AppSync GraphQL api.
-resource "aws_appsync_graphql_api" "appsync" {
-  name                = "${var.prefix}_appsync"
-  schema              = file("schema.graphql")
-  authentication_type = "API_KEY"
+  schema = <<EOF
+    type Query {
+      hello: String
+    }
+
+  EOF
 }
 
-# Create the API key.
-resource "aws_appsync_api_key" "appsync_api_key" {
-  api_id = aws_appsync_graphql_api.appsync.id
+resource "aws_appsync_datasource" "sample_datasource" {
+  api_id = aws_appsync_graphql_api.sample_appsync_api.id
+  name   = "SampleDataSource"
+  type   = "NONE"
 }
 
-# --- listPeople ---
-
-# Create zip file from Go list-people binary.
-data "archive_file" "listPeople_lambda_zip" {
-  type        = "zip"
-  source_file = "./bin/list-people"
-  output_path = "./zip/list-people.zip"
+resource "aws_appsync_resolver" "sample_resolver" {
+  api_id         = aws_appsync_graphql_api.sample_appsync_api.id
+  type           = "Query"
+  field          = "hello"
+  data_source    = aws_appsync_datasource.sample_datasource.name
+  request_template = <<EOF
+{
+  "version": "2017-02-28",
+  "operation": "Invoke",
+  "payload": {}
 }
+EOF
 
-# Create lambda function from zip file, with lambda role.
-resource "aws_lambda_function" "listPeople_lambda" {
-  function_name    = "${var.prefix}_listPeople_lambda"
-  filename         = data.archive_file.listPeople_lambda_zip.output_path
-  source_code_hash = data.archive_file.listPeople_lambda_zip.output_base64sha256
-  role             = aws_iam_role.iam_lambda_role.arn
-  runtime          = "go1.x"
-  handler          = "list-people"
+  response_template = <<EOF
+#set($inputRoot = $util.parseJson($ctx.stash.get("RESPONSE_STRING")))
+{
+  "message": "$inputRoot.message"
+}
+EOF
 }
