371b39bebddcadba48c103d4432f2865b0af8b29
rougraff <140021677+rougraff@users.noreply.github.com>
2023-08-09 13:29:55 -0400

    Update hello.go

--- functions/hello.go
+++ functions/hello.go
@@ -1,26 +1,39 @@
 package main
+
 import (
 	"context"
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+// Response struct defines the structure of the JSON response.
 type Response struct {
 	Message string `json:"message"`
 }
+
+// helloHandler is the main Lambda handler for API Gateway requests.
 func helloHandler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Create a Response struct with a custom message.
 	response := Response{
 		Message: "Hello, Gopher!",
 	}
+
+	// Marshal the Response struct into JSON format.
 	body, err := json.Marshal(response)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
+
+	// Construct an APIGatewayProxyResponse with a status code of 200 (OK)
+	// and the marshaled JSON response as the body.
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       string(body),
 	}, nil
 }
+
 func main() {
+	// Start the Lambda handler when the Lambda function is invoked.
 	lambda.Start(helloHandler)
 }
