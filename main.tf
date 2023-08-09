c1b537e4b00bd762c583f0216da29ee60e3d1c37
rougraff <140021677+rougraff@users.noreply.github.com>
2023-08-09 13:24:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+#This Terraform file defines the infrastructure for setting up an AWS AppSync API 
+
+#Specifies region
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
 }
-
+#Creates an AWS AppSync GraphQL API named "SampleAppSyncAPI" and defines schema
 resource "aws_appsync_graphql_api" "sample_appsync_api" {
   name                = "SampleAppSyncAPI"
   authentication_type = "API_KEY"  # You can change the authentication method as needed
@@ -14,12 +17,14 @@ resource "aws_appsync_graphql_api" "sample_appsync_api" {
   EOF
 }
 
+#Defines data source
 resource "aws_appsync_datasource" "sample_datasource" {
   api_id = aws_appsync_graphql_api.sample_appsync_api.id
   name   = "SampleDataSource"
   type   = "NONE"
 }
 
+#Sets up a resolver for the "hello" field in the "Query" type
 resource "aws_appsync_resolver" "sample_resolver" {
   api_id         = aws_appsync_graphql_api.sample_appsync_api.id
   type           = "Query"
